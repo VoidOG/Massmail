6ed317b162bf222edd832ca8b9f29db222d64f5d
VoidOG <VoidGod123@outlook.com>
2025-06-17 18:56:15 +0530

    Update Old.py

--- Old.py
+++ Old.py
@@ -6,7 +6,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
 import random
 
-BOT_TOKEN = '6795292888:AAE4Twu2yrZpKC8kbdkE5fCQvcOsBDfLId0'
+BOT_TOKEN = '6795292888:AAHTxIAjbpJxXZrlBv_6C_F3LhsTSyOQqGA'
 authorized_users = [6663845789, 6551446148, 6698364560, 1110013191]
 
 senders = [
